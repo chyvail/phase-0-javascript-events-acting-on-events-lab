d492952ac44ac701c984a0837f947f903675ba08
joshua chivile <joshuachivile@gmail.com>
2023-09-28 16:04:20 +0300

    complted dodger assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const moveDodgerLeft = () => {
+  const dodger = document.getElementById("dodger");
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+};
+
+moveDodgerLeft();
+
+const moveDodgerRight = () => {
+  const dodger = document.getElementById("dodger");
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+};
+
+moveDodgerRight();
